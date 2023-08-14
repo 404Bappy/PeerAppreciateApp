7e029aafebd14396f07c5c09395878dd8dae3003
Bappy71 <19103076@iubat.edu>
2023-08-14 12:35:27 +0600

    first commit

--- Components/FilterMenu/FilterMenu.js
+++ Components/FilterMenu/FilterMenu.js
@@ -1,29 +1,49 @@
-import { MenuProvider } from "react-native-popup-menu";
-import {
-  Menu,
-  MenuOptions,
-  MenuOption,
-  MenuTrigger,
-} from "react-native-popup-menu";
-import { View } from "react-native";
-import { Text } from "react-native-elements";
+// import { MenuProvider } from "react-native-popup-menu";
+// import {
+//   Menu,
+//   MenuOptions,
+//   MenuOption,
+//   MenuTrigger,
+// } from "react-native-popup-menu";
+// import { View } from "react-native";
+// import { Text } from "react-native-elements";
 
-export const FilterMenu = () => (
-  <View style={{ backgroundColor: "#5456" }}>
-    <Text>Hello world!</Text>
-    <Menu>
-      <MenuTrigger text="Select action" />
-      <MenuOptions>
-        <MenuOption onSelect={() => alert(`Save`)} text="HI There" />
-        <MenuOption onSelect={() => alert(`Delete`)}>
-          <Text style={{ color: "red" }}>Delete</Text>
-        </MenuOption>
-        <MenuOption
-          onSelect={() => alert(`Not called`)}
-          disabled={true}
-          text="Disabled"
-        />
-      </MenuOptions>
-    </Menu>
-  </View>
-);
+// export const FilterMenu = () => (
+//   <View style={{ backgroundColor: "#5456" }}>
+//     <Text>Hello world!</Text>
+//     <Menu>
+//       <MenuTrigger text="Select action" />
+//       <MenuOptions>
+//         <MenuOption onSelect={() => alert(`Save`)} text="HI There" />
+//         <MenuOption onSelect={() => alert(`Delete`)}>
+//           <Text style={{ color: "red" }}>Delete</Text>
+//         </MenuOption>
+//         <MenuOption
+//           onSelect={() => alert(`Not called`)}
+//           disabled={true}
+//           text="Disabled"
+//         />
+//       </MenuOptions>
+//     </Menu>
+//   </View>
+// );
+
+import { TouchableOpacity, View, Text } from "react-native";
+import React, { useState } from "react";
+
+export default function FilterMenu() {
+  const [visible, setVisible] = useState(false);
+  return (
+    <>
+      <TouchableOpacity>
+        <Icon name="tune" type="MaterialCommunityIcons" />
+      </TouchableOpacity>
+      <Modal trans={visible}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "red" }}
+          
+        ></SafeAreaView>
+      </Modal>
+    </>
+  );
+}
